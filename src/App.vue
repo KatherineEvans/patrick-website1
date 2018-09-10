3d41044d18e6e74b58a339f8c673666781608e97
KatherineEvans <Evans.Katherine.E@gmail.com>
2018-09-09 20:12:43 -0500

    CSS and HTML Additions

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,212 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+html {
+  font-size: 10px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /*resetting the browser settings */
+}
+
+body {
+  font-family: "Lato", sans-serif;
+  font-weight: 400;
+/*  font-size: 16px;*/
+  line-height: 1.7;
+  color: #777;
+  padding: 3rem;
+}
+
+.header {
+  height: 95vh;
+  /*95% of the viewport height */
+  background-image: linear-gradient(
+    to right bottom, 
+    rgba(126, 213, 111, 0.7), 
+    rgba(40, 180, 131, 0.7)), 
+  url(http://1lipics.com/img/single/2018/03/24/1lipics.com_Photo_2b573624711c5778c47def26d06c74e3.jpg);
+  background-size: cover;
+  /* fit image into width of viewport */
+  background-position: top;
+  position: relative;
+
+  clip-path: polygon(0% 0%, 100% 0%, 100% 55%, 0% 100%);
+
+}
+
+.logo-box {
+  position: absolute;
+  top: 4rem;
+  left: 4rem;
+}
+
+.logo {
+  height: 3.5rem;
+}
+
+.text-box {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  /*Transform/translate no longer looks at the box relative to the parent aka. the header. It looks at it relative to the object itself */
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.heading-primary {
+  color: #fff;
+  text-transform: uppercase;
+
+  backface-visibility: hidden;
+  /*The backface ivsibility keeps what's behind the element hidden - fixes shaking in animation*/
+  margin-bottom: 6rem;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.heading-primary-main {
+  display: block;
+  font-size: 6rem;
+  /*font size*/
+  font-weight: 400;
+  /*font thickness(ish)*/
+  letter-spacing: 3.5rem;
+  /*Space between letter, i.e. spread out or smushed*/
+  animation-name: moveInLeft;
+  animation-duration: 2s;
+  animation-timing-function: ease-out;
+  /* the number of times an animation runs
+  animation-iteration-count: 3;
+
+  the lenght of time before an animation begins
+  animation-delay: 0.5s;
+  */
+}
+
+.heading-primary-sub {
+  display: block;
+  font-size: 2rem;
+  font-weight: 700;
+  letter-spacing: 1.75rem;
+  animation: moveInRight 2s ease-out;
+  /*short hand version of writing what's written in heading-primary-main */
+  }
+
+/* to use css animation, you must first specifiy *keyframes* for the animation */
+@keyframes moveInLeft {
+  0% {
+    opacity: 0;
+    /* start of animation */
+    transform: translateX(-10rem);
+    /* use translateX because the x axis is the horizontal axis */
+  }
+
+  80% {
+    transform: translateX(1rem);
+  }
+
+  100% {
+    opacity: 1;
+    /* end of annimation */
+    transform: translate(0);
+    /*translating at 0 means it will look like it does form the start, with 0 animation */
+  }
+}
+
+@keyframes moveInRight {
+  0% {
+    opacity: 0;
+    /* start of animation */
+    transform: translateX(10rem);
+    /* use translateX because the x axis is the horizontal axis */
+  }
+
+  80% {
+    transform: translateX(-1rem);
+  }
+
+  100% {
+    opacity: 1;
+    /* end of annimation */
+    transform: translate(0);
+    /*translating at 0 means it will look like it does form the start, with 0 animation */
+  }
+}
+
+@keyframes moveInBottom {
+  0% {
+    opacity: 0;
+    /* start of animation */
+    transform: translateY(3rem);
+    /* use translateY because the y axis is the verticle axis, positive travels up, negative travels down */
+  }
+
+  100% {
+    opacity: 1;
+    /* end of annimation */
+    transform: translate(0);
+    /*translating at 0 means it will look like it does form the start, with 0 animation */
+  }
+}
+
+.btn:link, 
+.btn:visited {
+  text-transform: uppercase;
+  text-decoration: none;
+  padding: 1.5rem 4rem;
+  display: inline-block;
+  border-radius: 10rem;
+  transition: all .2s;
+  position: relative;
+  font-size: 1.6rem;
+} /* using the : followed by text (here "link") is the sudo class - used to style elements under a special condiiton. You can use the sudo class "visited" after a link has been clicked*/
+
+.btn:hover {
+  transform: translateY(-.3rem);
+  box-shadow: 0 1rem 2rem rgba(0,0,0,.2);
+  /*first value is X axis, second is Y axis, thrid is blur, fourth is color!*/
+}
+
+.btn:active {
+  transform: translateY(-.1rem);
+  box-shadow: 0 .5rem 1rem rgba(0,0,0,.2);
+}
+
+.btn-white {
+  background-color: #fff;
+  color: #777;
+  animation: moveInBottom 1s ease-out .75s;
+  animation-fill-mode: backwards; /*this will automatically apply the 0% before the animation starts*/
+}
+
+.btn::after {
+  content: "";
+  display: inline-block;
+  /*Display is set to inline-block because our button is set to inline-block. You must always specify content too, even if blank */
+  height: 100%;
+  width: 100%;
+  border-radius: 10rem;
+  position: absolute;
+  /*position is listed as absolute in relations to the closest position it can find which is the button's relative position*/
+  top: 0;
+  left: 0;
+  z-index: -1;
+  /* z-index defines positions of elements if they're stacked i.e. one on top of the other. AKA this goes behind, hence the -1!*/
+  transition: all .4s;
+}
+
+.btn-white::after {
+  background-color: #fff;
+}
+
+.btn:hover::after {
+  transform: scaleX(1.4) scaleY(1.4); /*scale 2 makes it double the size it was before*/
+  opacity: 0; /* fade out */
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home header">
+    <div class="logo-box">
+        <img src="img/pglogo2.png" class="logo">
+    </div>
+    <div class="text-box">
+    <h1 class="heading-primary">
+        <span class="heading-primary-main">Patrick Grimaldi</span>
+        <span class="heading-primary-sub">Blah blah blah</span>
+    </h1>
+
+    <a href="#" class="btn btn-white">Contact</a><!-- You can make two classes in one line using the space key -->
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
 </script>
